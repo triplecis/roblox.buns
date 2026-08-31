84bfe3b59526d7ca5cd9b32d751ff4825b810e58
tripping on stars <87574728+triplecis@users.noreply.github.com>
2026-08-31 01:19:14 +0000

    tabs are retarded

--- launch/scripts.lua
+++ launch/scripts.lua
@@ -8,10 +8,8 @@ print("Loading Scripts module...")
 
 --// Get Games //--
 print("Fetching all game scripts...")
-local Games = _Tabs.Scripts:AddLeftGroupbox("Games")
-local Undetected = Games:AddLeftTabbox("Undetected Games")
-local Detected = Games:AddRightTabbox("Detected Games")
+local Games = _Tabs.Scripts:AddLeftTabbox("Games")
 
-Undetected:AddLabel("Undetected Games")
-Detected:AddLabel("Detected Games")
+Games:AddTab("Undetected")
+Games:AddTab("Detected")
 
